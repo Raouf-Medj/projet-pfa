c23febee8887fdc870d12a5542be70d1064d1b6b
Zieir <zineddinekermadj123@gmail.com>
2025-03-19 17:37:37 +0100

    Corrected to have 2 types of doors : "g" for locked/ "G" for unlocked

--- src/components/gate.ml
+++ src/components/gate.ml
@@ -3,12 +3,13 @@ open System_defs
 
 type tag += Gate of gate
 
-let gate x y =
+let gate x y locked =
   let e = new gate () in
   e#texture#set Texture.black;
   e#position#set Vector.{x = float x; y = float y};
   e#tag#set (Gate e);
   e#box#set Rect.{width = Cst.gate_size; height = Cst.gate_size};
+  if (not locked) then (e#unlock);
   Draw_system.(register (e :> t));
   Collision_system.(register (e :> t));
   e
